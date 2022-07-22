abeead1b69f5d155b20df053831a998cce7c78bc
Seungjee Lee <moondgod@gmail.com>
2022-07-23 00:22:54 +0900

    SchoolSearch

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const locate = document.querySelector(".blank");
+const locates = document.querySelector(".Serach");
+function locateHandle(){
+    location.href="../html/school.html";
+}
+locate.addEventListener("click", locateHandle);
+locates.addEventListener("click", locateHandle);
