b64e81d7076c83b0126168b298fdd677e089335c
Seungjee Lee <moondgod@gmail.com>
2022-07-28 22:20:05 +0900

    날짜 받아와서 기본값, 최대값 JS로 수정하기

--- js/infect.js
+++ js/infect.js
@@ -6,6 +6,41 @@ function noteFill(){
     fillName.textContent = newSchool;
     fillSchool.textContent = newName;
     console.log(newName, newSchool);
+    
+    const ConfirmationDate = document.querySelector(".BirthInput .PWinput");
+    const testDate = document.querySelector(".PasswordInput .PWinput");
+    const ExpectedDate = document.querySelector("#ExpectedInput");
+    const ToDay = new Date();
+    const date = String(ToDay.getDate()).padStart(2, "0");
+    const year = String(ToDay.getFullYear()).padStart(2, "0");
+    const month = String(ToDay.getMonth()+1).padStart(2, "0");
+    const Sum = `${year}-${month}-${date}`;
+    console.log(String(Sum));
+    ConfirmationDate.value = Sum;
+    ConfirmationDate.max = Sum;
+    const Confirmationdate = String(ToDay.getDate()-6).padStart(2, "0");
+    const ConfirmationSum = `${year}-${month}-${Confirmationdate}`;
+    ConfirmationDate.min = ConfirmationSum;
+    const testdate = String(ToDay.getDate()-1).padStart(2, "0");
+    const testSum = `${year}-${month}-${testdate}`;
+    testDate.value = testSum;
+    testDate.min = ConfirmationSum;
+    testDate.max = Sum;
+    console.log(ToDay.getDate()+6);
+    if((ToDay.getDate()+6)>=31){
+        const Expectedmonth = String(ToDay.getMonth()+2).padStart(2, "0");
+        const Expecteddate = String(ToDay.getDate()-25).padStart(2, "0");
+        const ExpectedSum = `${year}-${Expectedmonth}-${Expecteddate}`;
+        console.log(ExpectedSum);
+        ExpectedDate.value = ExpectedSum;
+    }
+    else{
+        const Expecteddate = String(ToDay.getDate()+6).padStart(2, "0");
+        const ExpectedSum = `${year}-${month}-${Expecteddate}`;
+        console.log(ExpectedSum);
+        ExpectedDate.value = ExpectedSum;
+    }
 }
 
-    noteFill();
+noteFill();
+// setInterval(noteFill, 1000);
